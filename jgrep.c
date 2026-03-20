a62b4dbee5ad10f72eeb12cee7d85e1b420c503e
Jamie <jamielkylelan@outlook.com>
2026-03-20 19:38:39 +0000

    fixed bug were jgrep would not find files over 1024 lines big

--- jgrep.c
+++ jgrep.c
@@ -1,32 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #define BUFFSIZE 1024
 int main(int argc, char** argv) {
-    char buff[BUFFSIZE];
     if (argc < 2 || argc > 3) {
         fprintf(stderr, "Usage: jgrep <pattern> <file>\n");
-        return 1;
+        return 2;
     }
     FILE* file;
     if (argc == 3) {
         file = fopen(argv[2], "r");
         if (file == NULL) {
             perror("FATAL ERROR");
-            return 2;
+            return 3;
         }
     } else {
         file = stdin;
     }
-
-    while (fgets(buff, BUFFSIZE, file) != NULL) {
-        if (strstr(buff, argv[1]) != NULL) {
-            printf("%s", buff);
+    size_t capacity = BUFFSIZE;
+    char* buf = malloc(capacity);
+    for (;;) {
+        size_t len = 0;
+        int got_line = 0;
+        if (fgets(buf + len, capacity - len, file) == NULL) {
+            got_line = (len > 0);
+            break;
         }
-        else {
-            printf("NOT FOUND\n");
-            return 3;
+        len = strlen(buf);
+        if (buf[len-1] == '\n') {
+            got_line = 1;
+            break;
+        }
+        capacity *= 2;
+        char* tmp = realloc(buf, capacity);
+        if (tmp == NULL) {
+            perror("FATAL ERROR");
+            return 4;
+        }
+        buf = tmp;
+        if (got_line) {
+            break;
+        }
+        if (strstr(buf, argv[1]) != NULL) {
+            printf("%s", buf);
         }
     }
+    if (buf == NULL) {
+        perror("FATAL ERROR");
+        return 1;
+    }
+
+    free(buff);
     if (argc == 3) {
         fclose(file);
     }
